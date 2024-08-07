6f372f2f5d4b19a039c98450cd337478913d80ea
arnob-webber <billahamid26@gmail.com>
2024-08-06 22:27:47 -0700

    js problem

---
+++ Selecting from Many Options with Switch Statements/code.js
@@ -0,0 +1,30 @@
+function caseInSwitch(val) {
+  let answer;
+
+  switch (val) {
+    case 1:
+      answer = "alpha";
+      break;
+    case 2:
+      answer = "beta";
+      break;
+    case 3:
+      answer = "gamma";
+      break;
+    case 4:
+      answer = "delta";
+      break;
+    default:
+      answer = "unknown";
+      break;
+  }
+
+  return answer;
+}
+
+
+console.log(caseInSwitch(1)); 
+console.log(caseInSwitch(2)); 
+console.log(caseInSwitch(3)); 
+console.log(caseInSwitch(4)); 
+console.log(caseInSwitch(5)); 
