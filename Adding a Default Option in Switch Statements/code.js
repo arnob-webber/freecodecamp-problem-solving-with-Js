5c681139b9c96f4462a816cfddcca8dba2780165
arnob-webber <billahamid26@gmail.com>
2024-08-07 00:39:24 -0700

    js

---
+++ Adding a Default Option in Switch Statements/code.js
@@ -0,0 +1,19 @@
+let letter = 'a';
+let answer;
+
+switch (letter) {
+  case 'a':
+    answer = 'apple';
+    break;
+  case 'b':
+    answer = 'bird';
+    break;
+  case 'c':
+    answer = 'cat';
+    break;
+  default:
+    answer = 'stuff';
+    break;
+}
+
+console.log(answer); 
