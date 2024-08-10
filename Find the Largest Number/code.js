1c6055d5efe194383933fa24ce6f07a5dab64ade
arnob-webber <billahamid26@gmail.com>
2024-08-10 12:00:41 -0700

    saturday 10

---
+++ Find the Largest Number/code.js
@@ -0,0 +1,9 @@
+function findTheLarge(x, y, z) {
+    return Math.max(x, y, z)
+}
+
+let a =5 , b= 60
+sum = a * b
+const y = sum
+
+console.log( findTheLarge(20, sum, 40, 169))
--- String Concatenation/code.js
+++ String Concatenation/code.js
@@ -3,3 +3,7 @@ function combineStrings(str1, str2, str3) {
 }
 
 console.log(combineStrings("hi", "arnob", "web dev.")); 
+
+// how to combine two strings in JavaScript.
+// u can do that someother way but its a simple way
+// freecampcode easy js channelges
